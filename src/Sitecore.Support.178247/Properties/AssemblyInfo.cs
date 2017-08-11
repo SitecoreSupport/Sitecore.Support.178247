6e64133234e6f26c351aae871fdfa74a162fbe30
DK\obr <obr@sitecore.net>
2017-08-11 11:32:14 +0300

    Init

---
+++ src/Sitecore.Support.178247/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.178247")]
+[assembly: AssemblyProduct("Sitecore.Support.178247")]
+[assembly: ComVisible(false)]
