4e5654f92ea19f16496b4c551253fca1afdc52e4
DK\obr <obr@sitecore.net>
2017-08-14 12:12:56 +0300

    Commit

---
+++ src/Sitecore.Support.178247/IndexFieldUTCDateTimeValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.ContentSearch;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.Converters
+{
+    public class IndexFieldUTCDateTimeValueConverter : Sitecore.ContentSearch.Converters.IndexFieldUtcDateTimeValueConverter
+    {
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            //Sitecore.Support.178247
+            culture = CultureInfo.InvariantCulture;                      
+            return base.ConvertTo(context, culture, value, destinationType);
+            //
+        }
+    }
+}
